0e5c5ff34f02af013cb69540ac0a7ce1e627dd9d
davidechicco <davide.chicco@gmail.com>
2021-11-09 13:44:10 -0500

    edited perl script

--- perl_script_healthyControlsPresence.pl
+++ perl_script_healthyControlsPresence.pl
@@ -16,66 +16,69 @@ my $randomNumber = int rand(1000);
 # my $GEOcode = "GSE30174";
 my $GEOcode = "GSE38043";
 
-print "> > > healthyControlsPresence.r phase started\n";
+print "> > > lynx phase started\n";
 
-my $RscriptFile = './healthyControlsPresentInputParams.r';
-unless (-e $RscriptFile) {
+# lynx part
+my $lynxOutputFile="temp_lynx_output_${GEOcode}_rand${randomNumber}.txt";
+ print "Created file $lynxOutputFile\n";
 
-    # The script downloads the R script if it does not exist in the current folder
-    print "\n The \"$RscriptFile\" file doesn't exist and will be downloaded now\n";
-    system `curl https://raw.githubusercontent.com/davidechicco/healthyControlsPresenceChecker_repo/main/healthyControlsPresentInputParams.r -o healthyControlsPresentInputParams.r`;
+my $GEOurl = "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=${GEOcode}";
+print "Executing lynx --dump $GEOurl > $lynxOutputFile\n";
+system("lynx --dump $GEOurl > $lynxOutputFile");
 
+my $output_lynx = `grep "healthy control" $lynxOutputFile`;
+my $result_lynx_search = "";
+if ($output_lynx eq "")
+{
+        $result_lynx_search = "FALSE";
+}
+else
+{
+        $result_lynx_search = "TRUE";
 }
 
-my $rScriptOutputFile="temp_R_output_${GEOcode}_rand${randomNumber}.txt";
-print "Created file $rScriptOutputFile\n";
- 
-# Call to the R script that checks the presence of healthy controls and prints its output in the $rScriptOutputFile
-system("Rscript healthyControlsPresentInputParams.r $GEOcode TRUE > $rScriptOutputFile");
-
-# Call to awk in bash to read the last word of the last line of the temporary file, that can be TRUE or FALSE
-my $outcome_rscript = `awk 'END {print \$NF}' $rScriptOutputFile`;
-$outcome_rscript =~ s/\n//;
-
-# Print the final outcome: TRUE if healhty controls were found, FALSE otherwise.
-print "Outcome of healthyControlsPresent.r for $GEOcode: $outcome_rscript\n";
-
-# print "outcome_rscript=$outcome_rscript\n";
+print "Outcome of healthy controls search in the file downloaded by lynx for $GEOcode: $result_lynx_search \n";
 
 # Remove the temporary file created earlier
-system("rm $rScriptOutputFile;");
-print "Removed file $rScriptOutputFile\n";
-print "< < < healthyControlsPresence.r phase finished\n\n\n";
+system("rm $lynxOutputFile;");
+print "Removed file $lynxOutputFile\n";
+print "< < <  lynx phase finished\n\n\n";
 
-if($outcome_rscript eq "FALSE"){
+print "> > > healthyControlsPresence.r phase started\n";
 
-    print "> > > lynx phase started\n";
 
-    # lynx part
-    my $lynxOutputFile="temp_lynx_output_${GEOcode}_rand${randomNumber}.txt";
-    print "Created file $lynxOutputFile\n";
+if($result_lynx_search eq "FALSE"){
 
-    my $GEOurl = "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=${GEOcode}";
-    print "Executing lynx --dump $GEOurl > $lynxOutputFile\n";
-    system("lynx --dump $GEOurl > $lynxOutputFile");
+    my $RscriptFile = './healthyControlsPresentInputParams.r';
+    unless (-e $RscriptFile) {
 
-    my $output_lynx = `grep "healthy control" $lynxOutputFile`;
-    my $result_lynx_search = "";
-    if ($output_lynx eq "")
-    {
-        $result_lynx_search = "FALSE";
-    }
-    else
-    {
-        $result_lynx_search = "TRUE";
+        # The script downloads the R script if it does not exist in the current folder
+        print "\n The \"$RscriptFile\" file doesn't exist and will be downloaded now\n";
+        system `curl https://raw.githubusercontent.com/davidechicco/healthyControlsPresenceChecker_repo/main/healthyControlsPresentInputParams.r -o healthyControlsPresentInputParams.r`;
+
     }
 
-    print "Outcome of healthy controls search in the file downloaded by lynx for $GEOcode: $result_lynx_search \n";
+    my $rScriptOutputFile="temp_R_output_${GEOcode}_rand${randomNumber}.txt";
+    print "Created file $rScriptOutputFile\n";
+    
+    # Call to the R script that checks the presence of healthy controls and prints its output in the $rScriptOutputFile
+    system("Rscript healthyControlsPresentInputParams.r $GEOcode TRUE > $rScriptOutputFile");
+
+    # Call to awk in bash to read the last word of the last line of the temporary file, that can be TRUE or FALSE
+    my $outcome_rscript = `awk 'END {print \$NF}' $rScriptOutputFile`;
+    $outcome_rscript =~ s/\n//;
+
+    # Print the final outcome: TRUE if healhty controls were found, FALSE otherwise.
+    print "Outcome of healthyControlsPresent.r for $GEOcode: $outcome_rscript\n";
+
+    # print "outcome_rscript=$outcome_rscript\n";
 
     # Remove the temporary file created earlier
-    system("rm $lynxOutputFile;");
-    print "Removed file $lynxOutputFile\n";
-    print "< < <  lynx phase finished\n\n\n";
+    system("rm $rScriptOutputFile;");
+    print "Removed file $rScriptOutputFile\n";
+    print "< < < healthyControlsPresence.r phase finished\n\n\n";
+
+
 
 }
 
