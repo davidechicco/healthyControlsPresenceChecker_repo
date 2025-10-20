66e8a40a20f14dd5f78db65e8615ba4e314f8a2d
davidechicco <davide.chicco@gmail.com>
2025-10-20 09:27:26 +0200

    updated

--- installPackages.r
+++ installPackages.r
@@ -8,9 +8,9 @@ set.seed(11)
 cat(":: Installation of the R packages required ::\n\n")
 
 # Here we install the CRAN missing packages
-list.of.packages <- c("easypackages", "xml2", "markdown", "knitr", "rmarkdown", "pacman", "dplyr", "getGeneExpressionFromGEO") # other packages
+list.of.packages <- c("xml2", "markdown", "knitr", "rmarkdown", "pacman", "dplyr", "geneExpressionFromGEO", "pacman") # other packages
 new_packages_to_install <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new_packages_to_install)) install.packages(new_packages_to_install, repos="https://utstat.toronto.edu/cran/")
+if(length(new_packages_to_install)) install.packages(new_packages_to_install, repos="https://cran.mirror.garr.it/CRAN/")
 
 # Here we install the Bioconductor missing packages
 
@@ -26,7 +26,7 @@ if( length(bioCpackagesNotInstalled) ) {
     BiocManager::install(listOfBiocPackages[bioCpackagesNotInstalled])
 }
 
-library("easypackages")
-libraries(list.of.packages)
-libraries(listOfBiocPackages)
+library("pacman")
+if(length(list.of.packages) >= 1) p_load(list.of.packages, character.only = TRUE)
+if(length(listOfBiocPackages) >= 1) p_load(listOfBiocPackages, character.only = TRUE)
 
